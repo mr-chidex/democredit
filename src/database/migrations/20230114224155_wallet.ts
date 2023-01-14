bb2bea5f69ed578057ad012c25c89ef6a434a186
mr-chidex <mbahchiboy2@gmail.com>
2023-01-15 00:13:13 +0100

    create account migration

---
+++ src/database/migrations/20230114224155_wallet.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+
+const tableName = 'accounts';
+
+export async function up(knex: Knex): Promise<void> {
+  if (await knex.schema.hasTable(tableName)) {
+    return;
+  }
+
+  return knex.schema.createTable(tableName, (table) => {
+    table.uuid('id').primary().notNullable().defaultTo(knex.raw('(UUID())'));
+    table.bigInteger('walletId').notNullable().unique(); //serve as user wallet account number
+    table.foreign('id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+    table.double('balance').notNullable().defaultTo(0.0);
+    table.string('bankName').nullable(); //user bank name for cash withdrawals out of democredit app
+    table.string('accountName').nullable(); //user personal account name for cash withdrawals out of democredit app
+    table.string('accountNo').nullable(); //user personal account number for cash withdrawals out of democredit app
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(tableName);
+}

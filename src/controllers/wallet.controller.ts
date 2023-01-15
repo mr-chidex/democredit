a99f029d47371344558158ac18442aac8b6ec202
mr-chidex <mbahchiboy2@gmail.com>
2023-01-15 15:59:24 +0100

    clean up

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { WALLET } from '../models';
 import { walletService } from '../services';
 
 class WalletController {
--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,5 +1,3 @@
-import jwt from 'jsonwebtoken';
-
 export interface JWTTOKEN {
   userId?: string;
 }

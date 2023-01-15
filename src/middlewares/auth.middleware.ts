7dd24d3556975d3ce4ed688f6a50810cbee26a9a
mr-chidex <mbahchiboy2@gmail.com>
2023-01-15 15:54:19 +0100

    authentication middleware created

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,50 @@
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import config from '../config';
+import { db } from '../database/knexConfig';
+import { USER } from '../models';
+import { JWTTOKEN } from '../models/auth.model';
+
+class AuthMiddleware {
+  async auth(req: Request | any, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+
+    if (!authorization?.startsWith('Bearer'))
+      return res.status(401).json({
+        error: true,
+        message: 'Unauthorized access: Invalid token format',
+      });
+
+    const token = authorization.replace('Bearer ', '');
+
+    if (!token)
+      return res.status(401).json({
+        error: true,
+        message: 'Unauthorized access: Token not found',
+      });
+
+    try {
+      const decodeToken = jwt.verify(token, config.SECRET_KEY);
+
+      const user = await db<USER>('users')
+        .where({ id: (decodeToken as JWTTOKEN).userId })
+        .first();
+
+      if (!user) {
+        return res.status(401).json({
+          error: true,
+          message: 'Unauthorized access: Account does not exist',
+        });
+      }
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export const authMiddleware = new AuthMiddleware();

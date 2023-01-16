9175ce90915626840f70c3297141cc11fc0cddf4
mr-chidex <mbahchiboy2@gmail.com>
2023-01-16 17:29:45 +0100

    wallet transaction history migration added

---
+++ src/database/migrations/20230116161342_transaction_history.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+
+const tableName = 'transaction_history';
+
+export async function up(knex: Knex): Promise<void> {
+  if (await knex.schema.hasTable(tableName)) {
+    return;
+  }
+
+  return knex.schema.createTable(tableName, (table) => {
+    table.uuid('id').primary().notNullable().defaultTo(knex.raw('(UUID())'));
+    table.enu('type', ['DEBIT', 'CREDIT']).notNullable();
+    table.bigint('walletId').notNullable().unique();
+    table.foreign('walletId').references('walletId').inTable('wallets').onDelete('CASCADE').onUpdate('CASCADE');
+    table.double('amount').notNullable();
+    table.string('from').nullable();
+    table.string('to').nullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(tableName);
+}

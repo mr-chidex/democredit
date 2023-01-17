45f442dca470189567ac8d15016fdb6a1785c8c6
mr-chidex <mbahchiboy2@gmail.com>
2023-01-17 20:07:30 +0100

    test for auth service added

---
+++ src/tests/auth.service.spec.ts
@@ -0,0 +1,47 @@
+import { USER } from '../models';
+import { authService } from '../services';
+import { mockUser1 } from './mocks';
+
+describe('Auth Service', () => {
+  describe('Register user', () => {
+    it('should return error on invalid body params', async () => {
+      const body: USER = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      };
+
+      expect(authService.register(body)).rejects.toThrow();
+    });
+
+    it('should register new user when success criteria is met', async () => {
+      const body: USER = mockUser1;
+
+      const myMock = jest.fn();
+      const response = myMock.mockImplementation((body, {}) => {}).mockReturnValue({ success: true });
+
+      expect(response()).toHaveProperty('success');
+    });
+  });
+
+  describe('Login user', () => {
+    it('should return error on invalid body params', async () => {
+      const body: USER = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      };
+
+      expect(authService.login(body)).rejects.toThrow();
+    });
+
+    it('should login user and return token on success', async () => {
+      const myMock = jest.fn();
+      const response = myMock.mockImplementation((body, {}) => {}).mockReturnValue({ success: true, token: 'dksndks' });
+
+      expect(response()).toHaveProperty('token');
+    });
+  });
+});
--- src/tests/users.service.spec.ts
+++ src/tests/users.service.spec.ts
@@ -1,24 +1,8 @@
-import { authMiddleware } from '../middlewares';
-import { authService } from '../services';
-import { mockUser1 } from './mocks';
-
 describe('User service', () => {
-  let token: any;
-
-  beforeAll(() => {
-    //getting token for testing
-    token = authService.getToken({
-      ...mockUser1,
-    });
-  });
-
   it('should return user details on success', async () => {
     //**Mocking req and response */
-    const req = {
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    };
+
+    // where request header contains user token
 
     const myMock = jest.fn();
     const response = myMock.mockImplementation((req, {}) => {}).mockReturnValue({ success: true });
